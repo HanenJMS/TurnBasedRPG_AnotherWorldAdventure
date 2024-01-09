1e207890a9bf75de038cb383c949088b55d62183
HanenJMS <Hanensgamelab@gmail.com>
2024-01-08 23:30:25 -0500

    Adding Animation for unit. tweaking values for better feel.

--- Assets/Scripts/UnitSystem/Unit.cs
+++ Assets/Scripts/UnitSystem/Unit.cs
@@ -6,23 +6,30 @@ namespace AnotherWorldProject.UnitSystem
     public class Unit : MonoBehaviour
     {
         Vector3 targetPosition;
+        [SerializeField] Animator unitAnimator;
         NavMeshAgent agent;
         float minDistance = 1f;
         private void Awake()
         {
             agent = GetComponent<NavMeshAgent>();
         }
+        private void Start()
+        {
+            targetPosition = this.transform.position;
+        }
         private void Update()
         {
-
-            if(Input.GetMouseButtonDown(0))
+            unitAnimator.SetBool("isRunning", Vector3.Distance(targetPosition, this.transform.position) > agent.stoppingDistance);
+            if (Input.GetMouseButtonDown(0))
             {
                 Move(MouseWorld.GetMousePosition());
+                agent.speed = 2.957f;
             }
         }
         private void Move(Vector3 targetPosition)
         {
             this.targetPosition = targetPosition;
+            this.transform.LookAt(targetPosition);
             agent.SetDestination(targetPosition);
         }
     }
